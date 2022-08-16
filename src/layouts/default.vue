8a062013489d4b571b28e40204217e6ca7d8d0d7
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2022-08-16 14:39:30 +0530

    Initial vue 3 template

---
+++ src/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <nav class="text-center border p-3">The navbar</nav>
+        <main>
+            <router-view></router-view>
+        </main>
+        <footer class="text-center border  p-3">The footer</footer>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
