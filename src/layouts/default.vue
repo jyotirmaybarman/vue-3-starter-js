cb247de9eab5217a93ed2bba23e5141a88195642
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2022-10-01 13:00:16 +0530

    add vue i18n

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
-        <nav class="text-center border p-3">The navbar</nav>
+        <nav class="text-center border flex justify-center gap-4 p-3">
+            <p>The navbar with - current lang {{ t('lang') }}</p>
+            <div class="flex justify-center gap-4">
+                <button class="px-2 text-white rounded bg-blue-500 block" @click="locale =='ru' ? setLang('en'):setLang('ru')">{{ locale }}</button>
+            </div>
+        </nav>
         <main>
             <router-view></router-view>
         </main>
@@ -9,6 +14,15 @@
 </template>
 
 <script setup>
+import { useI18n } from 'vue-i18n';
+
+
+const { t, locale } = useI18n();
+
+function setLang(val){
+    locale.value = val;
+    
+}
 
 </script>
 
